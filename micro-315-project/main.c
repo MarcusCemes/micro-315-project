f646a3bc60c0bbd0b966300fe6a94b6534bf3347
Marcus Cemes <marcus.cemes@gmail.com>
2022-04-28 08:33:34 +0200

    feat: initialize IO

--- micro-315-project/main.c
+++ micro-315-project/main.c
@@ -3,15 +3,25 @@
 #include <string.h>
 
 #include <arm_math.h>
+#include <audio/audio_thread.h>
 #include <audio/microphone.h>
 #include <ch.h>
 #include <chprintf.h>
 #include <hal.h>
+#include <leds.h>
 #include <motors.h>
 #include <memory_protection.h>
+#include <sensors/mpu9250.h>
+#include <sensors/proximity.h>
+#include <sensors/VL53L0X/VL53L0X.h>
+#include <spi_comm.h>
 #include <usbcfg.h>
 
 #include "main.h"
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
 static void init(void);
 
@@ -21,8 +31,20 @@ int main(void)
 
     while (1)
     {
-        chThdSleepMilliseconds(1000);
+        chThdSleepMilliseconds(60000); // Sufficiently long duration
     }
+}
+
+static void serial_start(void)
+{
+    static SerialConfig ser_cfg = {
+        115200,
+        0,
+        0,
+        0,
+    };
+
+    sdStart(&SD3, &ser_cfg);
 }
 
 static void init(void)
@@ -30,6 +52,23 @@ static void init(void)
     halInit();   // HAL initialization
     chSysInit(); // ChibiOS/RT initialization
     mpu_init();  // Memory Protection initialization
+
+    // Initialize the inter-process communication bus
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+    // Reset to a known state
+    clear_leds();      // LEDS
+    set_body_led(0);   // LEDS
+    set_front_led(0);  // LEDS
+    usb_start();       // USB
+    dcmi_start();      // Camera
+    po8030_start();    // IC2, Camera
+    motors_init();     // Motors
+    proximity_start(); // Proximity sensors
+    dac_start();       // Speaker
+    spi_comm_start();  // Serial Peripheral Interface
+    serial_start();    // UART3
+    mic_start(NULL);   // Microphone
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
