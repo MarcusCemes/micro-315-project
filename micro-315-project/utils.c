d406b599b6310a4415024a197996bfe852f6387a
Marcus Cemes <marcus.cemes@gmail.com>
2022-05-05 14:34:08 +0200

    feat: readers-writer lock implementation

--- micro-315-project/utils.c
+++ micro-315-project/utils.c
@@ -1,6 +1,59 @@
 #include "utils.h"
 
+#include <ch.h>
+
 inline size_t min(size_t a, size_t b)
 {
     return a <= b ? a : b;
+}
+
+/* == Readers-Writer lock implementation == */
+
+struct rw_lock_t
+{
+    mutex_t mtx;
+    condition_variable_t cond;
+    int32_t readers;
+};
+
+void rw_init(rw_lock_t* lock)
+{
+    chMtxObjectInit(&lock->mtx);
+    chCondObjectInit(&lock->cond);
+    lock->readers = 0;
+}
+
+void rw_read_lock(rw_lock_t* lock)
+{
+    chMtxLock(&lock->mtx);
+    while (lock->readers == -1)
+        chCondWait(&lock->cond);
+    ++lock->readers;
+    chMtxUnlock(&lock->mtx);
+}
+
+void rw_read_unlock(rw_lock_t* lock)
+{
+    chMtxLock(&lock->mtx);
+    --lock->readers;
+    if (lock->readers == 0)
+        chCondBroadcast(&lock->cond);
+    chMtxUnlock(&lock->mtx);
+}
+
+void rw_write_lock(rw_lock_t* lock)
+{
+    chMtxLock(&lock->mtx);
+    while (lock->readers != 0)
+        chCondWait(&lock->cond);
+    lock->readers = -1;
+    chMtxUnlock(&lock->mtx);
+}
+
+void rw_write_unlock(rw_lock_t* lock)
+{
+    chMtxLock(&lock->mtx);
+    lock->readers = 0;
+    chCondBroadcast(&lock->cond);
+    chMtxUnlock(&lock->mtx);
 }
--- micro-315-project/utils.h
+++ micro-315-project/utils.h
@@ -3,7 +3,20 @@
 
 #include <stdlib.h>
 
-/** A simple Math.min() function implementation. */
+/**
+ * Implementation of a Readers–writer lock, favouring reads.
+ * Allows a resource to be safely accessed by multiple threads
+ * concurrently, such as when broadcasting data.
+ */
+typedef struct rw_lock_t rw_lock_t;
+
+/** Returns the smallest of two integers. */
 size_t min(size_t a, size_t b);
+
+void rw_init(rw_lock_t* lock);
+void rw_read_lock(rw_lock_t* lock);
+void rw_read_unlock(rw_lock_t* lock);
+void rw_write_lock(rw_lock_t* lock);
+void rw_write_unlock(rw_lock_t* lock);
 
 #endif
