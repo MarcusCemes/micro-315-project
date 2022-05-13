956dfac9bd95284f2dadd3caaaee19f6ed42974e
Marcus Cemes <marcus.cemes@gmail.com>
2022-05-13 22:00:36 +0200

    feat: motor step target, mutex sync, timer optimisations - Halt the motor when a step target has been reached - Synchronise access to static variables with a mutex - Avoid unnecessary pwm...() calls that may introduce timer delays

--- micro-315-project/motor_control.c
+++ micro-315-project/motor_control.c
@@ -13,13 +13,20 @@ typedef bool motor_state_t[4];
 /** Represents a motor, holding counters and configuration for that motor. */
 typedef struct
 {
-    uint16_t speed;
+    uint16_t step_speed;
     mctl_dir_t direction;
-    int32_t half_step_count;
     uint8_t step_index;
+    int32_t half_step_count;
+    bool half_step_target_enabled;
+    int32_t half_step_target;
+    threads_queue_t target_queue;
+    bool interrupt_enabled;
+    bool powersave_enabled;
 } motor_t;
 
-/* === Constants === */
+typedef PWMDriver timer_t;
+
+/* === Definitions === */
 
 #define MOTOR_TIMER_FREQ 100000  // [Hz]
 
@@ -29,9 +36,12 @@ typedef struct
 #define NB_PHASES 4             // The number of phases/pins a motor has
 
 #define ZERO_SPEED 0            // The speed at which the motor is stopped
+#define ZERO_COUNTER 0          // The initial counter state
 #define MAX_PERIOD UINT16_MAX   // The maximum counter value for a timer (16 bits)
 #define STEP_HALF_STEP_RATIO 2  // The ratio between a full step and a half step
 #define POWERSAVE_CHANNEL 0     // The timer channel used for powersaving
+
+/* === Constants === */
 
 /** Lookup table for motor step sequence. */
 static const motor_state_t _step_lut[NB_STEPS] = {
@@ -43,7 +53,7 @@ static const motor_state_t _step_lut[NB_STEPS] = {
 static const motor_state_t _state_halt = { 0, 0, 0, 0 };
 
 /** Pointers to timers corresponding to each motor. */
-static const PWMDriver *_motor_timers[NB_MOTORS] = { &PWMD3, &PWMD4 };
+static const timer_t *_motor_timers[NB_MOTORS] = { &PWMD3, &PWMD4 };
 
 /** The GPIO pins corresponding to each motor phase. */
 static const uint8_t _motor_pins[NB_MOTORS][4] = {
@@ -53,11 +63,17 @@ static const uint8_t _motor_pins[NB_MOTORS][4] = {
 
 /* === Static variables === */
 
+/** Lock for syncrhonising access to static variables. */
+static mutex_t _lock;
+
 /** The state for each of the motors phases, corresponding to a motor position. */
 static motor_t _motors[NB_MOTORS];
 
 /** Contains the PWM configuration for each motor. */
 static PWMConfig _motor_pwm_configs[NB_MOTORS];
+
+/** Queue for threads waiting for all motors to be halted. */
+static threads_queue_t _motor_halt_queue;
 
 /* === Private functions === */
 
@@ -91,6 +107,18 @@ static inline mctl_dir_t V2D(int16_t velocity)
     return velocity == ZERO_SPEED ? MC_HALT : velocity > ZERO_SPEED ? MC_FORWARD : MC_BACKWARD;
 }
 
+/** Get the motor associated with a side. */
+static inline motor_t *get_motor(mctl_side_t side)
+{
+    return &_motors[side];
+}
+
+/** Utility to cast timer to non-const for ChibiOS functions. */
+static inline timer_t *get_timer(mctl_side_t side)
+{
+    return (timer_t *)_motor_timers[side];
+}
+
 /* == GPIO == */
 
 /** Updates a single motor pin to a boolean value. */
@@ -108,8 +136,8 @@ static void update_motor_pins(mctl_side_t side, const motor_state_t state)
 }
 
 /** Returns the motor side that is associated with a particular timer. */
-static mctl_side_t side_from_timer(PWMDriver *timer) __attribute__((hot));
-static mctl_side_t side_from_timer(PWMDriver *timer)
+static mctl_side_t side_from_timer(timer_t *timer) __attribute__((hot));
+static mctl_side_t side_from_timer(timer_t *timer)
 {
     for (uint8_t i = 0; i < NB_MOTORS; ++i)
         if (_motor_timers[i] == timer)
@@ -119,67 +147,190 @@ static mctl_side_t side_from_timer(PWMDriver *timer)
     __builtin_unreachable();
 }
 
+/** Returns whether the step counter has reached the target count. */
+static bool motor_at_target(motor_t *motor)
+{
+    bool enabled = motor->half_step_target_enabled;
+    bool reached = motor->half_step_count == motor->half_step_target;
+    return enabled && reached;
+}
+
+/** Returns true if all motors are in the `MC_HALT` state. */
+static bool all_halted(void)
+{
+    for (uint8_t i = 0; i < NB_MOTORS; ++i)
+        if (_motors[i].direction != MC_HALT)
+            return false;
+
+    return true;
+}
+
 /* == Commands == */
 
-/** Immediately cut the current running to a motor. Does not update the state! */
+/** Immediately cut the current running to a motor, **without** updating the state. */
 static inline void halt_motor(mctl_side_t side)
 {
     update_motor_pins(side, _state_halt);
 }
 
-/** Immediately cut current to all motors. Does not update the state! */
+/** Immediately cut current to all motors, **without** updating the state. */
 static void halt_motor_all(void)
 {
     for (uint8_t i = 0; i < NB_MOTORS; ++i)
         halt_motor(i);
 }
 
-/** Enables PWM-based current reduction for the given motor. */
-static inline void motor_powersave_enable(mctl_side_t side)
+/**
+ * Enable/disable timer interrupts when the counter resets.
+ * Checks whether a change is needed to avoid resetting the timer.
+ */
+static void motor_set_interrupt_enabled(mctl_side_t side, bool enabled)
 {
-    pwmEnableChannel((PWMDriver *)_motor_timers[side], POWERSAVE_CHANNEL,
-                     (pwmcnt_t)(MOTOR_TIMER_FREQ / SPEED_THRESHOLD));
+    motor_t *motor = get_motor(side);
+    if (motor->interrupt_enabled == enabled)
+        return;
+
+    motor->interrupt_enabled = enabled;
+
+    timer_t *timer = get_timer(side);
+    if (enabled)
+        pwmEnablePeriodicNotification(timer);
+    else
+        pwmDisablePeriodicNotification(timer);
 }
 
-/** Disables PWM-based current reduction for the given motor. */
-static inline void motor_powersave_disable(mctl_side_t side)
+/** I-class variant to disable timer interrupts (used to shutdown from ISR). */
+static inline void motor_interrupt_disableI(mctl_side_t side)
 {
-    pwmDisableChannel((PWMDriver *)_motor_timers[side], POWERSAVE_CHANNEL);
+    motor_t *motor = get_motor(side);
+    if (!motor->interrupt_enabled)
+        return;
+
+    motor->interrupt_enabled = false;
+    pwmDisablePeriodicNotificationI(get_timer(side));
+}
+
+/**
+ * CH1 timer interrupts are used to invoke motor_powersave_cb().
+ * The invocation frequency is set to that of the main timer at SPEED_THRESHOLD.
+ */
+static void motor_set_powersave_enabled(mctl_side_t side, bool enabled)
+{
+    motor_t *motor = get_motor(side);
+    if (motor->powersave_enabled == enabled)
+        return;
+
+    motor->powersave_enabled = enabled;
+
+    timer_t *timer = get_timer(side);
+    const pwmcnt_t interrupt_period = MOTOR_TIMER_FREQ / SPEED_THRESHOLD;
+    if (enabled)
+        pwmEnableChannel(timer, POWERSAVE_CHANNEL, interrupt_period);
+    else
+        pwmDisableChannel(timer, POWERSAVE_CHANNEL);
+}
+
+/** I-class variant to disable powersave interrupts (shutdown during ISR). */
+static inline void motor_powersave_disableI(mctl_side_t side)
+{
+    motor_t *motor = get_motor(side);
+    if (!motor->powersave_enabled)
+        return;
+
+    motor->powersave_enabled = false;
+    pwmDisableChannelI(get_timer(side), POWERSAVE_CHANNEL);
+}
+
+/** Update the timer period for a given step speed. */
+static void update_timer_speed(mctl_side_t side, uint32_t step_speed)
+{
+    motor_t *motor = get_motor(side);
+    if (motor->step_speed == step_speed)
+        return;
+
+    motor->step_speed = step_speed;
+    pwmcnt_t counter = step_speed == ZERO_SPEED ? MAX_PERIOD : MOTOR_TIMER_FREQ / S2HS(step_speed);
+    pwmChangePeriod(get_timer(side), counter);
+}
+
+/** I-class function that does a complete motor shutdown. */
+static void motor_full_shutdownI(mctl_side_t side)
+{
+    motor_t *motor = get_motor(side);
+    timer_t *timer = get_timer(side);
+
+    halt_motor(side);
+
+    motor_interrupt_disableI(side);
+    motor_powersave_disableI(side);
+
+    motor->step_speed = ZERO_SPEED;
+    pwmChangePeriodI(timer, MAX_PERIOD);
+
+    chSysLockFromISR();
+
+    chThdDequeueAllI(&get_motor(side)->target_queue, MSG_OK);
+
+    if (all_halted())
+        chThdDequeueAllI(&_motor_halt_queue, MSG_OK);
+
+    chSysUnlockFromISR();
 }
 
 /* == Callbacks == */
 
-/** Callback to update the motor's state and GPIO pins. */
-static void motor_tick_cb(PWMDriver *timer)
+/**
+ * Invoked in a ISR when the main timer counter resets. Updates the motor
+ * state and the motor's GPIO pins for the next step configuration.
+ *
+ * If the motor has been marked as halted, the motor will undergo a full
+ * shutdown. Subsequent movement may have a significant (random) startup delay.
+ */
+static void motor_tick_cb(timer_t *timer)
 {
-    mctl_side_t side = side_from_timer(timer);
-    motor_t *motor = &_motors[side];
-
-    if (motor->direction == MC_HALT)
-    {
-        halt_motor(side);
+    // Bail from the ISR if the mutex is locked
+    if (_lock.m_cnt != 0)
         return;
+
+    mctl_side_t side = side_from_timer(timer);
+    motor_t *motor = get_motor(side);
+
+    if (motor_at_target(motor))
+        motor->direction = MC_HALT;
+
+    switch (motor->direction)
+    {
+        case MC_HALT:
+            motor_full_shutdownI(side);
+            break;
+
+        case MC_FORWARD:
+        case MC_BACKWARD:
+        {
+            int8_t dir_int = unit_xor(motor->direction == MC_FORWARD);
+            int8_t side_int = unit_xor(side == MC_LEFT);
+            int8_t index_inc = side_int * dir_int;
+            uint8_t step_index = fast_modulo_8((int8_t)motor->step_index + index_inc);
+
+            motor->step_index = step_index;
+            motor->half_step_count += dir_int;
+
+            update_motor_pins(side, _step_lut[step_index]);
+            break;
+        }
     }
-
-    int8_t dir_int = unit_xor(motor->direction == MC_FORWARD);
-    int8_t side_int = unit_xor(side == MC_LEFT);
-    int8_t index_inc = side_int * dir_int;
-    uint8_t step_index = fast_modulo_8((int8_t)motor->step_index + index_inc);
-
-    motor->step_index = step_index;
-    motor->half_step_count += dir_int;
-
-    update_motor_pins(side, _step_lut[step_index]);
 }
 
 /**
- * Callback invoked by CH1 of the PWM to cut current to motors during the
- * active part of the duty cycle to avoid high current draw at low speeds.
+ * Callback invoked by CH1 of the PWM timer to limit the current that
+ * is drawn by the motors at low speed. While the period of the PWM
+ * timer remains unchanged, CH1 will fire after the same amount of time
+ * as if the motor was turning at SPEED_THRESHOLD. Current is then
+ * reapplied at the next motor tick.
  */
-static void motor_powersave_cb(PWMDriver *timer)
+static void motor_powersave_cb(timer_t *timer)
 {
-    mctl_side_t side = side_from_timer(timer);
-    halt_motor(side);
+    halt_motor(side_from_timer(timer));
 }
 
 /* == Initialisation == */
@@ -187,12 +338,19 @@ static void motor_powersave_cb(PWMDriver *timer)
 /** Initialises the state structs for the given motor. */
 static void init_motor(mctl_side_t side)
 {
-    _motors[side] = (motor_t){
-        .speed = ZERO_SPEED,
+    motor_t *motor = get_motor(side);
+
+    *motor = (motor_t){
+        .step_speed = ZERO_SPEED,
         .direction = MC_HALT,
-        .half_step_count = 0,
+        .half_step_count = ZERO_COUNTER,
+        .half_step_target = ZERO_COUNTER,
         .step_index = 0,
+        .interrupt_enabled = false,
+        .powersave_enabled = false,
     };
+
+    chThdQueueObjectInit(&motor->target_queue);
 }
 
 /** Initialises the state struct for all motors. */
@@ -205,7 +363,7 @@ static void init_motor_all(void)
 /** Initialise the PWM timer for the given motor. */
 static void init_motor_pwm(mctl_side_t side)
 {
-    PWMDriver *timer = (PWMDriver *)_motor_timers[side];
+    timer_t *timer = get_timer(side);
     PWMConfig *timer_config = &_motor_pwm_configs[side];
 
     *timer_config =  (PWMConfig){
@@ -221,11 +379,8 @@ static void init_motor_pwm(mctl_side_t side)
         },
     };
 
-    // Configure the PWM peripheral
+    // Configure and start the timer
     pwmStart(timer, timer_config);
-
-    // Enable period-start interrupts, invoking the motor_tick() callback
-    pwmEnablePeriodicNotification(timer);
 }
 
 /** Initialise the PWM timer for all motors. */
@@ -239,14 +394,39 @@ static void init_motor_pwm_all(void)
 
 uint32_t mctl_get_counter(mctl_side_t side)
 {
-    motor_t *motor = &_motors[side];
-    return HS2S(motor->half_step_count);
+    chMtxLock(&_lock);
+    uint32_t steps = HS2S(get_motor(side)->half_step_count);
+    chMtxUnlock(&_lock);
+    return steps;
 }
 
 void mctl_set_counter(mctl_side_t side, uint32_t steps)
 {
-    motor_t *motor = &_motors[side];
-    motor->half_step_count = S2HS(steps);
+    chMtxLock(&_lock);
+    get_motor(side)->half_step_count = S2HS(steps);
+    chMtxUnlock(&_lock);
+}
+
+uint32_t mctl_get_target(mctl_side_t side)
+{
+    chMtxLock(&_lock);
+    uint32_t steps = HS2S(get_motor(side)->half_step_target);
+    chMtxUnlock(&_lock);
+    return steps;
+}
+
+void mctl_set_target(mctl_side_t side, uint32_t steps)
+{
+    chMtxLock(&_lock);
+    get_motor(side)->half_step_target = S2HS(steps);
+    chMtxUnlock(&_lock);
+}
+
+void mctl_set_target_enabled(mctl_side_t side, bool enabled)
+{
+    chMtxLock(&_lock);
+    get_motor(side)->half_step_target_enabled = enabled;
+    chMtxUnlock(&_lock);
 }
 
 void mctl_set_inverse_velocity(int16_t velocity)
@@ -261,32 +441,71 @@ void mctl_set_common_velocity(int16_t velocity)
     mctl_set_motor_velocity(MC_LEFT, velocity);
 }
 
+void mctl_rotate_by(mctl_side_t side, uint32_t steps, int16_t velocity)
+{
+    chMtxLock(&_lock);
+    motor_t *motor = get_motor(side);
+    motor->half_step_count = ZERO_COUNTER;
+    motor->half_step_target = unit_xor(velocity) * S2HS(steps);
+    mctl_set_motor_velocity(side, velocity);
+    chMtxUnlock(&_lock);
+}
+
 void mctl_set_motor_velocity(mctl_side_t side, int16_t velocity)
 {
-    motor_t *motor = &_motors[side];
-
+    motor_t *motor = get_motor(side);
     uint16_t step_speed = clamp(abs(velocity), ZERO_SPEED, MOTOR_SPEED_LIMIT);
-    motor->speed = step_speed;
+
+    chMtxLock(&_lock);
     motor->direction = V2D(velocity);
 
-    if (step_speed == ZERO_SPEED)
+    switch (step_speed)
     {
-        motor_powersave_disable(side);
-        pwmChangePeriod((PWMDriver *)_motor_timers[side], 1000);
-        return;
+        case ZERO_SPEED:
+            // Queue motor shutdown on next tick
+            motor->direction = MC_HALT;
+            break;
+
+        default:
+            motor_set_powersave_enabled(side, S2HS(step_speed) < SPEED_THRESHOLD);
+            update_timer_speed(side, step_speed);
+            motor_set_interrupt_enabled(side, true);
+            break;
     }
 
-    uint16_t half_step_speed = 2 * step_speed;
-    if (half_step_speed < SPEED_THRESHOLD)
-        motor_powersave_enable(side);
-    else
-        motor_powersave_disable(side);
+    chMtxUnlock(&_lock);
+}
 
-    pwmChangePeriod((PWMDriver *)_motor_timers[side], MOTOR_TIMER_FREQ / half_step_speed);
+void mctl_wait_halted(mctl_side_t side)
+{
+    motor_t *motor = get_motor(side);
+
+    chSysLock();
+    chMtxLockS(&_lock);
+
+    if (motor->direction != MC_HALT)
+        chThdEnqueueTimeoutS(&motor->target_queue, TIME_INFINITE);
+
+    chMtxUnlockS(&_lock);
+    chSysUnlock();
+}
+
+void mctl_wait_halted_all(void)
+{
+    chSysLock();
+    chMtxLockS(&_lock);
+
+    if (!all_halted())
+        chThdEnqueueTimeoutS(&_motor_halt_queue, TIME_INFINITE);
+
+    chMtxUnlockS(&_lock);
+    chSysUnlock();
 }
 
 void mctl_init(void)
 {
+    chMtxObjectInit(&_lock);
+    chThdQueueObjectInit(&_motor_halt_queue);
     init_motor_all();
     halt_motor_all();
 }
--- micro-315-project/motor_control.h
+++ micro-315-project/motor_control.h
@@ -1,6 +1,7 @@
 #ifndef MOTOR_CONTROL_H
 #define MOTOR_CONTROL_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef enum
@@ -23,11 +24,27 @@ uint32_t mctl_get_counter(mctl_side_t side);
 /** Sets the step counter value for a given motor. */
 void mctl_set_counter(mctl_side_t side, uint32_t steps);
 
+/** Gets the motor's step target count. */
+uint32_t mctl_get_target(mctl_side_t side);
+
+/** Sets the motor's step target count. */
+void mctl_set_target(mctl_side_t side, uint32_t steps);
+
+/**
+ * Actives/decactives the step target of a motor, after which
+ * the motor will automatically shutdown and notify any waiting
+ * threads.
+ */
+void mctl_set_target_enabled(mctl_side_t side, bool enabled);
+
 /** Set the angular velocity of both motors, with the left motor is inverted. */
 void mctl_set_inverse_velocity(int16_t velocity);
 
 /** Set the angular velocity of both motors. */
 void mctl_set_common_velocity(int16_t velocity);
+
+/** Rotate the motor a specific number of steps at a given velocity. */
+void mctl_rotate_by(mctl_side_t side, uint32_t steps, int16_t velocity);
 
 /**
  * Set the angular velocity [step/s] of a motor. Setting to zero will
@@ -35,6 +52,12 @@ void mctl_set_common_velocity(int16_t velocity);
  * enable a powersaving feature that uses a PWM duty cycle.
  */
 void mctl_set_motor_velocity(mctl_side_t side, int16_t velocity);
+
+/** Checks if the motor is stopped, and if not, blocks until it is halted. */
+void mctl_wait_halted(mctl_side_t side);
+
+/** Blocks until all motors are halted. */
+void mctl_wait_halted_all(void);
 
 /** Initialise motor state and cut current to motors. */
 void mctl_init(void);
